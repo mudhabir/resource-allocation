723169e01bda15479799a48310a66d3c4add75f9
mudhabir <77278870+mudhabir@users.noreply.github.com>
2021-01-14 16:35:36 +0530

    Full logic for resource allocation and refactoring

--- lib/resource_allocation.rb
+++ lib/resource_allocation.rb
@@ -1,4 +1,6 @@
 class ResourceAllocation
+  CPU_SIZE = ['large', 'xlarge', '2xlarge', '4xlarge', '8xlarge', '10xlarge']
+
   CPU_COUNT = {
     'large'=> 1,
     'xlarge'=> 2,
@@ -7,31 +9,32 @@ class ResourceAllocation
     '8xlarge'=> 16,
     '10xlarge'=> 32,
   }
-  def initialize
-    # We can update the cost of instances, add or remove instances in a region
-    @resources = {
-      us_east: {
-        'large'=> 0.12,
-        'xlarge'=> 0.23,
-        '2xlarge'=> 0.45,
-        '4xlarge'=> 0.774,
-        '8xlarge'=> 1.4,
-        '10xlarge'=> 2.82
-      },
-      us_west: {
-        'large'=> 0.14,
-        '2xlarge'=> 0.413,
-        '4xlarge'=> 0.89,
-        '8xlarge'=> 1.3,
-        '10xlarge'=> 2.97
-      },
-      asia: {
-        'large'=> 0.11,
-        'xlarge'=> 0.20,
-        '4xlarge'=> 0.67,
-        '8xlarge'=> 1.18
-      }
+
+  # We can update the cost of instances, add or remove instances in a region
+  RESOURCES = {
+    us_east: {
+      'large'=> 0.12,
+      'xlarge'=> 0.23,
+      '2xlarge'=> 0.45,
+      '4xlarge'=> 0.774,
+      '8xlarge'=> 1.4,
+      '10xlarge'=> 2.82
+    },
+    us_west: {
+      'large'=> 0.14,
+      '2xlarge'=> 0.413,
+      '4xlarge'=> 0.89,
+      '8xlarge'=> 1.3,
+      '10xlarge'=> 2.97
+    },
+    asia: {
+      'large'=> 0.11,
+      'xlarge'=> 0.25,
+      '4xlarge'=> 0.67,
+      '8xlarge'=> 1.18
     }
+  }
+  def initialize
     @unit_cpu_price = price_per_cpu
     p 'Enter usage hours:'
     hours = gets.to_i
@@ -39,57 +42,86 @@ class ResourceAllocation
     cpus = gets.to_i
     p 'Enter the maximum price:'
     price = gets.to_f
-    get_costs(hours, cpus, price)
+    p get_costs(hours, cpus, price)
   end
 
   def price_per_cpu
     unit_cpu_price = []
-    # Calculate the cost for 1 CPU for each of the instance
-    @resources.each do |region, instances|
-      unit_cpu_price << ["#{region}-large", @resources[region]['large']] if @resources[region]['large']
-      unit_cpu_price << ["#{region}-xlarge", @resources[region]['xlarge'] / CPU_COUNT['xlarge']] if @resources[region]['xlarge']
-      unit_cpu_price << ["#{region}-2xlarge", @resources[region]['2xlarge'] / CPU_COUNT['2xlarge']] if @resources[region]['2xlarge']
-      unit_cpu_price << ["#{region}-4xlarge", @resources[region]['4xlarge'] / CPU_COUNT['4xlarge']] if @resources[region]['4xlarge']
-      unit_cpu_price << ["#{region}-8xlarge", @resources[region]['8xlarge'] / CPU_COUNT['8xlarge']] if @resources[region]['8xlarge']
-      unit_cpu_price << ["#{region}-10xlarge", @resources[region]['10xlarge'] / CPU_COUNT['10xlarge']] if @resources[region]['10xlarge']
+    # Calculate the cost for 1 CPU for each of the instance. This will help in fetching servers with minimum cost
+    RESOURCES.each do |region, instances|
+      CPU_SIZE.each do |size|
+        unit_cpu_price << ["#{region}-#{size}", RESOURCES[region][size] / CPU_COUNT[size]] if RESOURCES[region][size]
+      end
     end
     unit_cpu_price.sort_by { |pair| pair[1] }
   end
 
   def get_costs(hours, cpus, price)
-    soln = []
+    result = []
     region_wise_data = {}
     if price == 0 # if price is not a constraint
-      @unit_cpu_price.each do |resource|
-        region, instance = resource[0].split('-')
-        instance_count = cpus.to_i / CPU_COUNT[instance].to_i
-        next if instance_count == 0
-
-        region_wise_data[region] = { 'total_cost' => 0, 'servers' => [] } if region_wise_data[region].nil?
-        region_wise_data[region]['total_cost'] += @resources[region.to_sym][instance] * instance_count * hours
-        region_wise_data[region]['servers'] << [instance, instance_count]
-        cpus -= (CPU_COUNT[instance] * instance_count)
-        break if cpus == 0
-      end
+      cpu_based(region_wise_data, hours, cpus)
     elsif cpus == 0 # if CPU is not a constraint
-      @unit_cpu_price.each do |resource|
-        region, instance = resource[0].split('-')
-        cost_per_server = @resources[region.to_sym][instance] * hours
-        instance_count = (price / cost_per_server).to_i
-        next if instance_count == 0
-
-        region_wise_data[region] = { 'total_cost' => 0, 'servers' => [] } if region_wise_data[region].nil?
-        region_wise_data[region]['total_cost'] += (cost_per_server * instance_count)
-        region_wise_data[region]['servers'] << [instance, instance_count]
-        price -= (cost_per_server * instance_count)
-        break if price == 0
-      end
+      cost_based_data(region_wise_data, hours, price)
     else
-      @unit_cpu_price.each do |resource|
-        region, instance = resource[0].split('-')
-      end
+      # allocate CPUs within given price, check if allocation possible, else return
+      alloc_possible = cpu_and_cost_based(region_wise_data, hours, cpus, price)
+      return 'cannot assign servers at this price' unless alloc_possible
     end
-    p region_wise_data
+    region_wise_data.each do |region, data|
+      data['region'] = region
+      result << data
+    end
+    result
+  end
+
+  def cpu_based(region_wise_data, hours, price)
+    @unit_cpu_price.each do |resource|
+      region, instance = resource[0].split('-')
+      cost_per_server = RESOURCES[region.to_sym][instance] * hours
+      instance_count = cpus / CPU_COUNT[instance]
+      next if instance_count == 0
+
+      region_data_calc(region_wise_data, region, instance, cost_per_server, instance_count)
+      cpus -= (CPU_COUNT[instance] * instance_count)
+      break if cpus == 0
+    end
+  end
+
+  def cost_based(region_wise_data, hours, price)
+    @unit_cpu_price.each do |resource|
+      region, instance = resource[0].split('-')
+      cost_per_server = RESOURCES[region.to_sym][instance] * hours
+      instance_count = (price / cost_per_server).to_i
+      next if instance_count == 0
+
+      region_data_calc(region_wise_data, region, instance, cost_per_server, instance_count)
+      price -= (cost_per_server * instance_count)
+      break if price == 0
+    end
+  end
+
+  def cpu_and_cost_based(region_wise_data, hours, cpus, price)
+    @unit_cpu_price.each do |resource|
+      region, instance = resource[0].split('-')
+      cost_per_server = RESOURCES[region.to_sym][instance] * hours
+      instance_needed = cpus.to_i / CPU_COUNT[instance].to_i
+      # instance count assigned should me minimum of instance needed or maximum instances that can be bought from the price
+      instance_count = [(price / cost_per_server).to_i, instance_needed].min
+      next if instance_count == 0
+
+      region_data_calc(region_wise_data, region, instance, cost_per_server, instance_count)
+      price -= (cost_per_server * instance_count)
+      cpus -= (CPU_COUNT[instance] * instance_count)
+      break if price == 0 || cpus <= 0
+    end
+    return cpus <= 0
+  end
+
+  def region_data_calc(region_wise_data, region, instance, cost_per_server, instance_count)
+    region_wise_data[region] = { 'total_cost' => 0, 'servers' => [] } if region_wise_data[region].nil?
+    region_wise_data[region]['total_cost'] += (cost_per_server * instance_count)
+    region_wise_data[region]['servers'] << [instance, instance_count]
   end
 end
 
